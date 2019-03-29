985214d82213fe7c4b5b6e1a8e5fc4cec47b95f0
LiulietLee <lee.liuliet@hotmail.com>
2019-03-29 18:19:30 +0800

    init project

---
+++ LLVector/LLVector/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LLVector
+//
+//  Created by Liuliet.Lee on 29/3/2019.
+//  Copyright © 2019 Liuliet.Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ src/LLVector.swift
@@ -0,0 +1,9 @@
+//
+//  LLVector.swift
+//  LLVector
+//
+//  Created by Liuliet.Lee on 29/3/2019.
+//  Copyright © 2019 Liuliet.Lee. All rights reserved.
+//
+
+import Foundation
